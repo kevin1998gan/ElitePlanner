5b83054fb3337ac5bbbe5ff3ca73abddc18fac39
kevin1998gan <kevin98_gan@yahoo.com>
2020-12-15 15:03:02 +0800

    added delete function

---
+++ ElitePlanner/assets/php/deleteTask.php
@@ -0,0 +1,19 @@
+<?php
+
+    $id = $_POST['id'];
+    //database details
+    $dbHost     = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName     = 'ElitePlanner';
+    
+    //create connection and select DB
+    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    if($db->connect_error){
+        die("Unable to connect database: " . $db->connect_error);
+    }
+    
+    //update user data from the database
+    $query = $db->query("CALL delete_Task('$id')");
+
+?>
