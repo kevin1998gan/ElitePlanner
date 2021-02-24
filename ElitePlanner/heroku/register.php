1a3773a733d15b1979e50f87c4acbd16ec0b8767
kevin1998gan <kevin98_gan@yahoo.com>
2021-02-24 14:26:41 +0800

    Test Heroku

---
+++ ElitePlanner/heroku/register.php
@@ -0,0 +1,17 @@
+<?php
+
+    $fname = $_POST['in_fname'];
+    $lname = $_POST['in_lname'];
+    $email = $_POST['in_email'];
+    $password = md5($_POST["in_password"]);
+
+    if($db->connect_error){
+        die("Unable to connect database: " . $db->connect_error);
+    }
+    
+    //update user data from the database
+    $query = $db->query("CALL insert_User('$fname', '$lname', '$email', '$password')");
+
+
+
+?>
