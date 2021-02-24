a292b601c0a1cb6b0336a41ad814ce992aa5e9bb
kevin1998gan <kevin98_gan@yahoo.com>
2021-02-24 16:11:09 +0800

    test Heroku connection

--- ElitePlanner/assets/heroku/test.php
+++ ElitePlanner/assets/heroku/test.php
@@ -1,13 +1,19 @@
 <?php
-    include 'testHeroku.php';
+$dsn = "pgsql:"
+    . "host=ec2-3-214-3-162.compute-1.amazonaws.com;"
+    . "dbname=deebgt7apro3pu;"
+    . "user=kriqhejgayywti;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=4f607a0f2d151b2c543417d36e171889bae280575034c526d10c7fdb352f3f7e";
 
-    
-    //update user data from the database
-    $query = $db->query("SELECT * FROM users");
-    $rows = array();
-    while($r = mysqli_fetch_assoc($query)) {
-        $rows[] = $r;
-    }
-    
-    //returns data as JSON format
-    echo json_encode($rows);
+$db = new PDO($dsn);
+//update user data from the database
+$query = $db->query("SELECT * FROM users");
+$rows = array();
+while ($r = mysqli_fetch_assoc($query)) {
+    $rows[] = $r;
+}
+
+//returns data as JSON format
+echo json_encode($rows);
