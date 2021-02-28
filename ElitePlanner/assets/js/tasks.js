a2c1bfb4e38dcc2050d685abb9577e874f5c42b9
kevin1998gan <kevin98_gan@yahoo.com>
2021-02-28 17:11:40 +0800

    Fixed tasks reminder modal

--- ElitePlanner/assets/js/tasks.js
+++ ElitePlanner/assets/js/tasks.js
@@ -387,7 +387,6 @@ tasks.prototype = {
 
             }
 
-            console.log(session_variables.reminder);
             if (comingSoon > 0 && session_variables.reminder == 0) {
                 $.ajax({
                     url: 'assets/php/updateReminder.php',
